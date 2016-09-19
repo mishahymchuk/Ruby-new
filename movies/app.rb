3f4740bbba5671c4f8e6b28210a1f1ba3dc486ce
Misha Hymchuk <misha.hymchuk@gmail.com>
2016-09-20 00:19:11 +0300

    Movies list

--- movies/app.rb
+++ movies/app.rb
@@ -1,15 +1,11 @@
 require 'sinatra'
 require 'movie'
-require 'yaml/store'
+require 'movie_store'
+
+store = MovieStore.new ('movies.yml')
 
   get ('/movies') do
-  	 @movies = []   
-  	 @movies[0] = Movie.new   
-  	 @movies[0].title = "Jaws"   
-  	 @movies[1] = Movie.new   
-  	 @movies[1].title = "Alien"   
-  	 @movies[2] = Movie.new   
-  	 @movies[2].title = "Terminator 2"
+  	 @movies = store.all
   	erb :index
   end
   get ('/movies/new') do
@@ -20,4 +16,6 @@ require 'yaml/store'
   	 @movie.title = params['title']   
   	 @movie.director = params['director']   
   	 @movie.year = params['year'] 
+     store.save (@movie)
+     redirect '/movies/new'
   end 
